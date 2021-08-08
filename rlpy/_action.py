0a482d43cebfe6f839155793df99ffa0268e08da
ctgk <r1135nj54w@gmail.com>
2021-08-09 06:48:19 +0900

    EDIT: action values are tuple values

--- rlpy/_action.py
+++ rlpy/_action.py
@@ -4,7 +4,7 @@ from enum import Enum
 class Action(Enum):
     """Class for enumerations of agent actions."""
 
-    UP = 1
-    DOWN = -1
-    LEFT = 2
-    RIGHT = -2
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
